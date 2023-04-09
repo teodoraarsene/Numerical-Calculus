9c65b7ffe6eefba9fae7ee2e46dc7b92e107836b
teodoraarsene <teodora.arsene01@gmail.com>
2023-04-09 21:11:50 +0300

    removed splines package and commented not working part in ex3 lab6

--- labs/lab6/vapor_pressure_3.m
+++ labs/lab6/vapor_pressure_3.m
@@ -30,39 +30,41 @@ hold on;
 plot(range, polyval(p2, range), 'g-');
 hold on;
 plot(range, polyval(p3, range), 'b-');
-hold on;
+##hold on;
 
-function w_i = compute_W_i(i, x)
-  n = length(x);
-  p = 1;
-  for j = 1:n
-    if i ~= j
-      p = p * (x(i) - x(j));
-    endif
-  end
-  w_i = 1 / p;
-endfunction
+# interpolation polynomial does not work :(
 
-function L = lagrange(x, y, xx)  
-  n = length(x);
-  N = length(xx);
-  
-  P = zeros(1, n);
-  L = zeros(1, N);
-  
-  for j = 1:N
-    s1 = 0;
-    s2 = 0;
-    for i = 1:n
-      P(i) = compute_W_i(i, x);
-      s1 = s1 + P(i) * y(i) / (xx(j) - x(i));
-      s2 = s2 + P(i) / (xx(j) - x(i));
-    endfor
-    
-  L(j) = s1 / s2;
-  end
-endfunction
-
-x = linspace(0, 10, 100);
-res = lagrange(x, f(x), t);
-plot(t, res, 'o');
+##function w_i = compute_W_i(i, x)
+##  n = length(x);
+##  p = 1;
+##  for j = 1:n
+##    if i ~= j
+##      p = p * (x(i) - x(j));
+##    endif
+##  end
+##  w_i = 1 / p;
+##endfunction
+##
+##function L = lagrange(x, y, xx)  
+##  n = length(x);
+##  N = length(xx);
+##  
+##  P = zeros(1, n);
+##  L = zeros(1, N);
+##  
+##  for j = 1:N
+##    s1 = 0;
+##    s2 = 0;
+##    for i = 1:n
+##      P(i) = compute_W_i(i, x);
+##      s1 = s1 + P(i) * y(i) / (xx(j) - x(i));
+##      s2 = s2 + P(i) / (xx(j) - x(i));
+##    endfor
+##    
+##  L(j) = s1 / s2;
+##  end
+##endfunction
+##
+##x = linspace(0, 10, 100);
+##res = lagrange(x, f(x), t);
+##plot(t, res, 'o');
